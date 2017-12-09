37c7df3883c801e2afeed9586c323d1d6c9d7ae8
Ahy14 <hello@anthonypauwels.be>
2017-12-10 00:29:55 +0100

    Modified dev.js

--- dev.js
+++ dev.js
@@ -1,11 +1,6 @@
-// var conf = require('./webpack.base.js')('dev');
-//
-// module.exports = conf;
-
 var webpack = require('webpack');
 var ora = require('ora');
 var conf = require('./webpack.base.js')('dev');
-var spinner = ora('Loading ...');
 
 webpack(conf, function(error, stats){
     if(error){
